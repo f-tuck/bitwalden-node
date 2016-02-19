892dd38f6c714a34f4c749cbda87bf5b306b9e48
Friar Tuck <f.tuck@gmx.com>
2016-02-19 16:18:07 +0800

    Preparing for discovery integration by turning off the manual announce. Messages show ports.

--- src/sharewode_node/core.cljs
+++ src/sharewode_node/core.cljs
@@ -53,7 +53,9 @@
                         (.on wire "close" (fn [] (print "Wire closed!") (print "wires:" (.-length (.-wires swarm)))))
                         (.on wire "keep-alive" (fn [] (print "Wire keepalive.")))))
     
-    (.on swarm "listening" (fn [] (js/console.log "Swarm listening.")))
+    (.on swarm "listening" (fn []
+                             (js/console.log "Swarm listening.")
+                             (print "Swarm port:" (.-port (.address swarm)))))
     (.on swarm "error" (fn [] (js/console.log "Swarm error.")))
     (.on swarm "close" (fn [] (js/console.log "Swarm close.")))
     
@@ -62,29 +64,33 @@
     (.on dht "ready"
          (fn []
            (print "DHT bootstrapped.")
-
            ;  {"peers" (.toArray dht)
            ;  "nodeId" (.toString (.-nodeId dht) "hex")}
 
            ; swarm.removePeer('127.0.0.1:42244') // remove a peer
 
-           (.lookup dht test-hash (fn [err node-count] (print "DHT lookup node count:" node-count)))
+           ; (.lookup dht test-hash (fn [err node-count] (print "DHT lookup node count:" node-count)))
+
            (.on dht "peer"
                 (fn [peer infoHash from]
                   (print "on peer: " peer "infoHash:" (.toString infoHash "hex") "from:" from)
-                  (.addPeer swarm (str (aget peer "host") ":6881"))))
+                  ; (.addPeer swarm (str (aget peer "host") ":6881"))
+                  
+                  ))
 
-           (.announce dht test-hash (fn [error] (if error
-                                                  (print "DHT announce error.")
-                                                  (print "DHT announce success."))))
+           ;(.announce dht test-hash (fn [error] (if error
+           ;                                       (print "DHT announce error.")
+           ;                                       (print "DHT announce success."))))
 
            ; (.on dht "node" (fn [node] (print "DHT node: "  (.toString (aget node "id") "hex") " " (aget node "host") ":" (aget node "port") " " (aget node "distance"))))
            ; (.on dht "warning" (fn [err] (print "DHT warning: " err)))
            (.on dht "announce" (fn [peer infoHash] (print "on announce (peer):" peer (.toString infoHash "hex"))))))
 
-    (.listen dht (fn [] (print "DHT listening.")))
-    (.listen swarm 6881)
-
+    (.listen dht (fn []
+                   (print "DHT listening.")
+                   (print "DHT port:" (.-port (.address dht)))))
+    (.listen swarm)
+    
     ; https://stackoverflow.com/a/14032965
     ; do something when app is closing
     (.on js/process "exit" (.bind exit-fn nil {:cleanup true}))

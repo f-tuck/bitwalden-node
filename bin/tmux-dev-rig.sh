10ed21dc3122cce3628177230d5a3052b744d194
Friar Tuck <f.tuck@gmx.com>
2017-09-12 13:21:31 +0000

    Wait for figwheel to start before launching dev server.

--- bin/tmux-dev-rig.sh
+++ bin/tmux-dev-rig.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 . ./deps/node/bin/activate
-tmux new-session -d "rlwrap lein figwheel"
-tmux split-window -v "while [ 1 ]; do ./bin/start-server-node.sh --dev; done"
+tmux new-session -d "echo 'Starting figwheel...'; rlwrap lein figwheel"
+wait_for_figwheel='echo "Waiting for figwheel..."; while [ "`netstat -ntlp 2>/dev/null | grep 3449`" == "" ]; do sleep 2; done; sleep 2'
+tmux split-window -v "$wait_for_figwheel; while [ 1 ]; do ./bin/start-server-node.sh --dev; done"
 tmux select-layout even-vertical
 tmux -2 attach-session -d

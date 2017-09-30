d4f46d4db61a761d4340ff6431ce777692537639
Friar Tuck <f.tuck@gmx.com>
2017-09-30 10:44:03 +0000

    String keynames in contracts for json consistency.

--- src/bitwalden_node/contracts.cljs
+++ src/bitwalden_node/contracts.cljs
@@ -11,32 +11,32 @@
 (defn dht-add [clients timestamp dht-put-params & [remaining]]
   (let [remaining (or remaining const/dht-refresh-count)]
     (if (> remaining 0)
-      (let [address [:contracts :refresh (get dht-put-params "k") (get dht-put-params "salt")]
+      (let [address [:contracts "refresh" (get dht-put-params "k") (get dht-put-params "salt")]
             ; find the index of existing k,salt refresh contract, if any
             existing (get-in clients address)
             clients-updated (if (or (not existing) (> (get dht-put-params "seq") ))
                               (update-in clients
                                          address
                                          assoc
-                                         :put-params (into {} (for [k ["v" "k" "salt" "seq" "s.dht"]]
+                                         "put-params" (into {} (for [k ["v" "k" "salt" "seq" "s.dht"]]
                                                                 [k (get dht-put-params k)])))
                               clients)]
         (update-in clients-updated address assoc 
-                   :last-refresh timestamp
+                   "last-refresh" timestamp
                    ; TODO: should be kb with POW use
                    ; one month every hour
-                   :remaining remaining))
-      (let [clients (update-in clients [:contracts :refresh (get dht-put-params "k")] dissoc (get dht-put-params "salt"))]
-        (if (> (count (get-in clients [:contracts :refresh (get dht-put-params "k")])) 0)
+                   "remaining" remaining))
+      (let [clients (update-in clients [:contracts "refresh" (get dht-put-params "k")] dissoc (get dht-put-params "salt"))]
+        (if (> (count (get-in clients [:contracts "refresh" (get dht-put-params "k")])) 0)
           clients
-          (update-in clients [:contracts :refresh] dissoc (get dht-put-params "k")))))))
+          (update-in clients [:contracts "refresh"] dissoc (get dht-put-params "k")))))))
 
 (defn dht-get-due [clients]
-  (let [dht-contracts (get-in clients [:contracts :refresh])]
+  (let [dht-contracts (get-in clients [:contracts "refresh"])]
     (filter (fn [[k salt contract]]
-              (< (contract :last-refresh) (- (timestamp-now) const/dht-refresh-interval-ms)))
+              (< (contract "last-refresh") (- (timestamp-now) const/dht-refresh-interval-ms)))
             (for [[k contracts] dht-contracts [salt contract] contracts]
-              [k salt contract (contract :remaining)]))))
+              [k salt contract (contract "remaining")]))))
 
 (defn dht-refresh-data [bt contracts-to-refresh]
   ; TODO: run these dht gets in parallel instead

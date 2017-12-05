ed43c926d0b7a4503f84b5f88cf2338aedc0cd06
Friar Tuck <f.tuck@gmx.com>
2017-12-05 00:30:12 +0000

    Fix stderr redirect in binary.

--- bin/binary-unpack-header.sh
+++ bin/binary-unpack-header.sh
@@ -4,7 +4,7 @@ found=`type node`
 if [ "$?" = "0" ]
 then
   mkdir -p ~/.bitwalden/logs
-  sed -e '0,/^#GZIPPED-BINARY-FOLLOWS#$/d' $0 | gunzip -c | node 2>&1 >~/.bitwalden/log/bitwalden.log
+  sed -e '0,/^#GZIPPED-BINARY-FOLLOWS#$/d' $0 | gunzip -c | node >~/.bitwalden/log/bitwalden.log 2>&1
   exit $?
 else
   echo "Can't find node binary in the PATH."

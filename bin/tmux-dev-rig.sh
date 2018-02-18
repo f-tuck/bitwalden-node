d1b297ad6dff974a930247c670f4be479010fbc8
Friar Tuck <f.tuck@gmx.com>
2018-02-18 12:35:40 +0000

    Wait longer for figwheel in tmux dev.

--- bin/tmux-dev-rig.sh
+++ bin/tmux-dev-rig.sh
@@ -2,7 +2,7 @@
 
 . ./deps/node/bin/activate
 tmux new-session -d "echo 'Starting figwheel...'; rlwrap lein figwheel"
-wait_for_figwheel='echo "Waiting for figwheel..."; while [ "`netstat -ntlp 2>/dev/null | grep 3450`" == "" ]; do sleep 2; done; sleep 2'
-tmux split-window -v "$wait_for_figwheel; while [ 1 ]; do ./bin/start-server-node.sh --dev; done"
+wait_for_figwheel='echo "Waiting for figwheel..."; while [ "`netstat -ntlp 2>/dev/null | grep 3450`" == "" ]; do sleep 2; done; sleep 3'
+tmux split-window -v "$wait_for_figwheel; while [ 1 ]; do ./bin/start-server-node.sh --dev; sleep 3; done"
 tmux select-layout even-vertical
 tmux -2 attach-session -d

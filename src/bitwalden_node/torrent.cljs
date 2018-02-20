ac367bed4ded19363014adce35bfc3764c865ac0
Friar Tuck <f.tuck@gmx.com>
2018-02-20 02:06:19 +0000

    Do not add/seed torrents if already added.

--- src/bitwalden_node/torrent.cljs
+++ src/bitwalden_node/torrent.cljs
@@ -16,55 +16,6 @@
 ; nodejs requirements
 (defonce debug (debug-fn "bitwalden-node.bittorrent"))
 
-; seed some content as well as listening out for gossip messages
-(defn seed [bt content-name contents downloads-dir]
-  (let [c (chan)]
-    (go
-      (let [content (js/Buffer. contents)]
-        (set! (.-name content) content-name)
-        ; have to do the create-torrent parse-torrent dance to get the infoHash
-        ; so that we have the correct download folder
-        (create-torrent
-          content
-          (fn [err torrent-blob]
-            (let [pre-torrent (parse-torrent torrent-blob)
-                  infoHash (.-infoHash pre-torrent)
-                  torrent (.seed bt content #js {:path (str downloads-dir "/" infoHash) :createdBy constants/created-by}
-                                 (fn [torrent]
-                                   (debug "Seeding" infoHash downloads-dir)
-                                   (put! c {"infohash" (.-infoHash torrent)})
-                                   (close! c)))])))))
-    c))
-
-; get a download link to some torrent
-(defn add [bt infoHash downloads-dir]
-  (let [c (chan)
-        path (str downloads-dir "/" infoHash)]
-    (go
-      (debug "Adding" infoHash downloads-dir)
-      (let [opts #js {:path path}]
-        (.add bt infoHash opts
-              (fn [torrent]
-                (put! c {"download" "starting"})
-                (go
-                  ; TODO: timeout if it appears to be hanging
-                  (loop []
-                    ;(print "Checking download progress")
-                    (when (put! c {"download" "progress" "value" (.-progress torrent)})
-                      (<! (timeout 1000))
-                      (if (< (.-progress torrent) 1.0)
-                        (recur)
-                        (do
-                          ;(put! c {"download" "progress" "value" 1})
-                          (put! c {"download" "done" "files" (map (fn [f] (into {} (map (fn [field] [field (aget f field)]) ["name" "path" "length"]))) (.-files torrent))})
-                          (close! c))))))))))
-    c))
-
-; re-seed a torrent we have on disk already
-(defn re-seed [bt infohash torrent-files path]
-  (debug "Re-seeding" infohash)
-  (<<< #(.seed bt (clj->js torrent-files) #js {:path path})))
-
 ; get a list of torrent hash directories from the downloads directory
 (defn get-disk-torrent-list [downloads-dir]
   (let [c (chan)]
@@ -86,3 +37,70 @@
 ; set of currently active torrent hashes
 (defn get-current-torrent-hashes [bt]
   (set (map #(aget % "infoHash") (.-torrents bt))))
+
+; extract salient file fields from torrent
+(defn get-torrent-files [torrent]
+  (map
+    (fn [f]
+      (into {} (map (fn [field] [field (aget f field)]) ["name" "path" "length"]))) 
+    (.-files torrent)))
+
+; seed some content
+(defn seed [bt content-name contents downloads-dir]
+  (let [c (chan)]
+    (go
+      (let [content (js/Buffer. contents)]
+        (set! (.-name content) content-name)
+        ; have to do the create-torrent parse-torrent dance to get the infoHash
+        ; so that we have the correct download folder
+        (create-torrent
+          content
+          (fn [err torrent-blob]
+            (let [pre-torrent (parse-torrent torrent-blob)
+                  infoHash (.-infoHash pre-torrent)]
+              (if (.get bt infoHash)
+                (do
+                  (debug "Already seeding" infoHash downloads-dir)
+                  (put! c {"infohash" infoHash "duplicate" true})
+                  (close! c))
+                (.seed bt content #js {:path (str downloads-dir "/" infoHash) :createdBy constants/created-by}
+                       (fn [torrent]
+                         (debug "Seeding" infoHash downloads-dir)
+                         (put! c {"infohash" (.-infoHash torrent)})
+                         (close! c)))))))))
+    c))
+
+; get a download link to some torrent
+(defn add [bt infoHash downloads-dir]
+  (let [c (chan)
+        path (str downloads-dir "/" infoHash)]
+    (go
+      (let [existing-torrent (.get bt infoHash)]
+        (if existing-torrent
+          (do
+            (debug "Already added" infoHash downloads-dir)
+            (put! c {"download" "done" "files" (get-torrent-files existing-torrent)}))
+          (let [opts #js {:path path}]
+            (debug "Adding" infoHash downloads-dir)
+            (.add bt infoHash opts
+                  (fn [torrent]
+                    (put! c {"download" "starting"})
+                    (go
+                      ; TODO: timeout if it appears to be hanging
+                      (loop []
+                        ;(print "Checking download progress")
+                        (when (put! c {"download" "progress" "value" (.-progress torrent)})
+                          (<! (timeout 1000))
+                          (if (< (.-progress torrent) 1.0)
+                            (recur)
+                            (do
+                              ;(put! c {"download" "progress" "value" 1})
+                              (put! c {"download" "done" "files" (get-torrent-files torrent)})
+                              (close! c))))))))))))
+    c))
+
+; re-seed a torrent we have on disk already
+(defn re-seed [bt infohash torrent-files path]
+  (debug "Re-seeding" infohash)
+  (<<< #(.seed bt (clj->js torrent-files) #js {:path path})))
+

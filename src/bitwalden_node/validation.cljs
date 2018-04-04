7cc438f9276a7caf69d05c05615cdbf7867af630
Friar Tuck <f.tuck@gmx.com>
2018-04-04 03:01:39 +0000

    Indent nit.

--- src/bitwalden_node/validation.cljs
+++ src/bitwalden_node/validation.cljs
@@ -3,53 +3,53 @@
             ["bs58" :as bs58]))
 
 (def check-fns {:exists?
-             (fn [p n] 
-               (when (nil? (p n))
-                 "must be present."))
+                (fn [p n] 
+                  (when (nil? (p n))
+                    "must be present."))
 
-             :base58-key?
-             (fn [p n]
-               (let [k (try (bs58/decode (p n)) (catch :default e nil))]
-                 (cond (nil? k)
-                       "must be base58 encoded."
-                       (not= (.-length k) 32)
-                       "must be 32 bytes when decoded.")))
+                :base58-key?
+                (fn [p n]
+                  (let [k (try (bs58/decode (p n)) (catch :default e nil))]
+                    (cond (nil? k)
+                          "must be base58 encoded."
+                          (not= (.-length k) 32)
+                          "must be 32 bytes when decoded.")))
 
-             :string?
-             (fn [p n]
-               (when (not= (type (p n)) js/String)
-                 "must be a string."))
+                :string?
+                (fn [p n]
+                  (when (not= (type (p n)) js/String)
+                    "must be a string."))
 
-             :max1k?
-             (fn [p n]
-               (when (>= (.-length (p n)) 1000)
-                 "must be less than 1000 bytes."))
+                :max1k?
+                (fn [p n]
+                  (when (>= (.-length (p n)) 1000)
+                    "must be less than 1000 bytes."))
 
-             :hex-sha1?
-             (fn [p n]
-               (let [k (try (js/Buffer. (p n) "hex") (catch :default e nil))]
-                 (cond
-                   (nil? k)
-                   "must be hex encoded."
-                   (not= (.-length (p n)) 40)
-                   "must be the correct length for a hex encoded sha1 hash.")))
-             
-             :hex-signature?
-             (fn [p n]
-               (let [k (try (js/Buffer. (p n) "hex") (catch :default e nil))]
-                 (cond
-                   (nil? k)
-                   "must be hex encoded."
-                   (not= (.-length (p n)) 128)
-                   "must be the correct length for a hex encoded signature.")))
-             
-             :int?
-             (fn [p n]
-               (let [k (p n)]
-               (when (and
-                       (not= (type k) js/Number) 
-                       (= (int k) k))
-                 "must be an integer")))})
+                :hex-sha1?
+                (fn [p n]
+                  (let [k (try (js/Buffer. (p n) "hex") (catch :default e nil))]
+                    (cond
+                      (nil? k)
+                      "must be hex encoded."
+                      (not= (.-length (p n)) 40)
+                      "must be the correct length for a hex encoded sha1 hash.")))
+
+                :hex-signature?
+                (fn [p n]
+                  (let [k (try (js/Buffer. (p n) "hex") (catch :default e nil))]
+                    (cond
+                      (nil? k)
+                      "must be hex encoded."
+                      (not= (.-length (p n)) 128)
+                      "must be the correct length for a hex encoded signature.")))
+
+                :int?
+                (fn [p n]
+                  (let [k (p n)]
+                    (when (and
+                            (not= (type k) js/Number) 
+                            (= (int k) k))
+                      "must be an integer")))})
 
 (defn check [params param-checks]
   (some (fn [[k checks]]
